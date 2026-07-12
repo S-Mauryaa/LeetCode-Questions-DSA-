66e35c3e66da0ab3077359fdcb03d58ec60dc8b5
Saurabh Maurya <github.saurabhmaurya@gmail.com>
2026-07-12 17:53:56 +0530

    Time: 1 ms (34.7%), Space: 28.3 MB (10.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,10 +3,8 @@ public:
     int majorityElement(vector<int>& nums) {
        sort(nums.begin(), nums.end());
 
-int n = nums.size();
-int mid = n / 2;
 
-return nums[mid];
+return nums[(nums.size())/2];
 
     }
 };
