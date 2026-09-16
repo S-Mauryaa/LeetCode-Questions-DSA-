b769fbbd6bc910ff853b09b120e413430982c178
Saurabh Maurya <github.saurabhmaurya@gmail.com>
2026-09-16 18:45:13 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        //  Easy method
+        // int n=nums.size();
+        // vector<bool> check(n+1,false);
+        // for(int i=0;i<n;i++){
+        //     int ele=nums[i];
+        //     check[ele]=true;
+        // }    
+        // for(int i=0;i<=n;i++){
+        //     if(check[i]==false) return i;
+        //     }
+        //     return 0;
+        
+        // using Cyclic Search
         int n=nums.size();
-        vector<bool> check(n+1,false);
+        int i=0;
+        while(i<n){
+            int correctIdx=nums[i];
+            if(correctIdx==i || nums[i]==n) i++;
+            else swap(nums[i],nums[correctIdx]);
+        }   
         for(int i=0;i<n;i++){
-            int ele=nums[i];
-            check[ele]=true;
-        }    
-        for(int i=0;i<=n;i++){
-            if(check[i]==false) return i;
-            }
-            return 0;
+            if(nums[i]!=i)return i;
+        }
+        return n;
     }
+
 };
