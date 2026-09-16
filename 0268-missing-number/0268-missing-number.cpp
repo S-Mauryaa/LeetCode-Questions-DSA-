df02b7e2b586b9bf2b3fa555b6839fad29192813
Saurabh Maurya <github.saurabhmaurya@gmail.com>
2026-09-16 18:49:41 +0530

    Time: 0 ms (100%), Space: 21.9 MB (12.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -15,24 +15,24 @@ public:
 
         // using Cyclic Search
         int n=nums.size();
-        // int i=0;
-        // while(i<n){
-        //     int correctIdx=nums[i];
-        //     if(correctIdx==i || nums[i]==n) i++;
-        //     else swap(nums[i],nums[correctIdx]);
-        // }   
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]!=i)return i;
-        // }
-        // return n;
+        int i=0;
+        while(i<n){
+            int correctIdx=nums[i];
+            if(correctIdx==i || nums[i]==n) i++;
+            else swap(nums[i],nums[correctIdx]);
+        }   
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i)return i;
+        }
+        return n;
 
         // sortest method
-        int actualsum=n*(n+1)/2;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        return actualsum-sum;
+        // int actualsum=n*(n+1)/2;
+        // int sum=0;
+        // for(int i=0;i<n;i++){
+        //     sum+=nums[i];
+        // }
+        // return actualsum-sum;
 
     }
 
